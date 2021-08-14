037174f785f63cc40761ec05733b8d2922c9d31f
MehmetEfeGosterici <m.efegost558@gmail.com>
2021-08-15 00:11:00 +0300

    frontend  render

--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ app.use("/api",router);
 
 app.use(express.static(path.join(__dirname, "../client/build")));
 
-app.get('/', function (req, res) {
+app.get('*', function (req, res) {
   res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
 });
 
